3234e48dc4f658a633ddbbba2d4d5a2da7729655
nickgg11 <nicholas.gingras@ocsbstudent.ca>
2017-09-18 10:31:29 -0400

    Added base program
    Added movement, points, map, camera movement.

---
+++ Roll-a-Ball First project/Assets/Scripts/Point_Bob.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point_Bob : MonoBehaviour {
+    private Rigidbody rb;
+    public float bump=0.5f;
+    private float timer = 0f;
+    Vector3 bounce;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        bounce = new Vector3(0, bump, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer += Time.deltaTime;
+        if (timer > 3)
+        {
+            
+            rb.AddForce(bounce , ForceMode.Impulse);
+            timer = 0;
+        }
+            
+        
+        
+	}
+}
